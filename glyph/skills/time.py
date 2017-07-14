93a845aa22e97c47c606e3660274cfbb9803543c
Ian Moore <ianmoore@greenms.com>
2017-07-14 13:34:58 -0400

    Error handling for unknown timezone

--- glyph/skills/time.py
+++ glyph/skills/time.py
@@ -6,14 +6,16 @@ import pytz
 
 
 def get_time_embed(timezone=None):
-    if timezone:
-        title = "{} Time".format(timezone)
-        timestamp = datetime.utcnow().replace(tzinfo=pytz.timezone(timezone))
-        time = datetime.now(tz=pytz.timezone(timezone))
-    else:
-        title = "UTC Time"
-        timestamp = datetime.utcnow()
-        time = datetime.utcnow()
+    title = "UTC Time"
+    # timestamp = datetime.utcnow()
+    time = datetime.utcnow()
+    try:
+        if timezone:
+            # timestamp = datetime.utcnow().replace(tzinfo=pytz.timezone(timezone))
+            time = datetime.now(tz=pytz.timezone(timezone))
+            title = "{} Time".format(timezone)
+    except pytz.UnknownTimeZoneError:
+        pass
     embed = discord.Embed(title=title, description=time.strftime("**%H:%M:%S** on %A %B %d %Y"))
     try_tz = random.choice(pytz.common_timezones)
     embed.set_footer(text="Time Skill | Try asking \"{} time\"".format(try_tz))
